e469854bca892649f7ad92b4f026d0d29ff8ea39
[상품개발팀] 김지예 <jiyea.kim@lotte.net>
2022-07-25 22:10:25 +0900

    테스트 소스 수정

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -4,31 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-
 @ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
-		int answer = -1;
-
-		int[] nums = {1,2,3,4};
-
-
-		for(int i=0;i<nums.length;i++){
-
-
-		}
-		// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-		System.out.println("answer " + answer);
-
 	}
 
 }
